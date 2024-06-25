153b54c9b8b3d00c7dd3f64d6e69c3d6c75e4066
v_bocbchen <v_bocbchen@tencent.com>
2024-06-25 16:43:46 +0800

    feature:图片和字体 样式

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { inter } from '@/app/ui/fonts';
+// import "./globals.css";
+import '@/app/ui/global.css';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
